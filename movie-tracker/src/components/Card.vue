8d249276b94f1e0825db325fb411fbaed52b023b
PYREX2020 <david.nicolas.selyem@gmail.com>
2026-05-11 15:23:02 +0300

    FINISHED

---
+++ movie-tracker/src/components/Card.vue
@@ -0,0 +1,17 @@
+
+<script setup lang="ts">
+defineProps({
+  name: String
+})
+</script>
+<template>
+    <div class="card">{{ name }}</div>
+</template>
+
+<style>
+.card {
+        padding:10px;
+        margin:10px;
+        border:1px solid red;
+}
+</style>
